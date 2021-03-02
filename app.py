c65208c65afacaef4d2723facb4f31e576e43e25
Sam <78932387+mnopqr1@users.noreply.github.com>
2021-03-02 11:09:57 +0100

    fixed bug and added draw possibility

--- app.py
+++ app.py
@@ -14,16 +14,23 @@ app.secret_key = b'\xda\x98c\xc1J\x05\x9d\xc3\xdc\xcb\xce0g\x1d\xd9-'
 
 OTHER = {"X" : "O", "O" : "X" }
 
+def restart_game():
+    session["board"] = [[None, None, None], [None, None, None], [None, None, None]]
+    session["turn"] = "X"
+    session["active"] = True
+    session["nmoves"] = 0
+    session["draw"] = False
+
 @app.route("/")
-def index(reset=False):
-    if "board" not in session or reset:
-        session["board"] = [[None, None, None], [None, None, None], [None, None, None]]
-        session["turn"] = "X"
-        session["active"] = True
+def index():
+    if "board" not in session:
+        restart_game()
     return render_template("game.html", 
         game=session["board"],
         turn=session["turn"],
-        active=session["active"])
+        active=session["active"],
+        draw=session["draw"],
+        n=session["nmoves"])
 
 def has_won(player):
     board = session["board"]
@@ -42,14 +49,20 @@ def has_won(player):
 @app.route("/play/<int:row>/<int:col>")
 def play(row, col):
     session["board"][row][col] = session["turn"]
+    session["nmoves"] += 1
     
     if has_won(session["turn"]):
         session["status"] = "Player " + session["turn"] + " has won! Game over."
         session["active"] = False
     else:
-        session["turn"] = OTHER[session["turn"]]
+        if session["nmoves"] == 9:
+            session["draw"] = True
+            session["active"] = False
+        else:
+            session["turn"] = OTHER[session["turn"]]
     return redirect(url_for("index"))
 
 @app.route("/reset")
 def reset():
-    return redirect(url_for("index"), reset=True)
+    restart_game()
+    return redirect(url_for("index", reset=True))
